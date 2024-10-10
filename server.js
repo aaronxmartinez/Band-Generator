02bba78b8a09504243635bc3e7f28a220c016115
Aaron <aaronmartinezz019@gmail.com>
2024-10-10 17:30:56 -0500

    added import path, import fileToUrl, add const __filename and __dirname, changed app.use(express...) in server.js file

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.render("index");
