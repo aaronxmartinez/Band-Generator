5e946cf5d428729d92fd514bcab7aa92a8b2495d
Aaron <aaronmartinezz019@gmail.com>
2024-10-10 15:34:03 -0500

    add server.js, different ejs files, and npm modules

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+const words = "adding a random variable to test git";
